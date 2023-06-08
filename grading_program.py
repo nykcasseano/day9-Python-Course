7a3c9355e16e77dbba105afb202596c098737a66
nyk <nykcasseano13@gmail.com>
2023-06-08 16:25:57 -0300

    último commit

--- grading_program.py
+++ grading_program.py
@@ -1,3 +1,11 @@
 student_score = {
-    
+   "Henrique": 71,
+   "Ronaldo": 81,
+   "Pedrinho": 91,
+   "Diego": 52,
+   "Naldo": 98
 }
+
+#Create a empty dictionary to receive the grades
+
+students_grades = {}
