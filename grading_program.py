83dc4a6228ccb1acd6213fc53c58bf5ec38c14e4
nyk <nykcasseano13@gmail.com>
2023-06-09 09:59:56 -0300

    último commit

--- grading_program.py
+++ grading_program.py
@@ -9,3 +9,19 @@ student_score = {
 #Create a empty dictionary to receive the grades
 
 students_grades = {}
+
+#write for loop
+
+for student in student_score:
+   score = student_score[student]
+   if score > 90:
+      students_grades[student] = "Excelente!"
+   elif score > 80:
+      students_grades[student] = "Muito Bom!"
+   elif score > 70:
+      students_grades[student] = "Isso ai!"
+   else:
+      students_grades[student] = "Não foi tão bem!"
+      
+      
+print(students_grades)
